44afbf8e1c9fe7f089c44ac83153e7a9431eb7ab
Hassan Syyid <hassansyyid@gmail.com>
2024-07-31 17:26:38 -0400

    Remove top param

--- tap_sap_b1/streams.py
+++ tap_sap_b1/streams.py
@@ -10,7 +10,7 @@ from tap_sap_b1.client import SAPB1Stream
 
 class InvoicesStream(SAPB1Stream):
     name = "invoices"
-    path = "/PurchaseInvoices?$select=DocEntry,DocNum,DocType,DocTotal,DocDate&$orderby=DocDate&$top=30"
+    path = "/PurchaseInvoices?$select=DocEntry,DocNum,DocType,DocTotal,DocDate&$orderby=DocDate"
     primary_keys = ["DocNum"]
     replication_key = "DocDate"
 
